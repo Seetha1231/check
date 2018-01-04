f6d99ab7c0a532e22448ebb03ed35fdb9981a9bf
Seetha1231 <34570365+Seetha1231@users.noreply.github.com>
2018-01-04 17:45:00 +0530

    Create hunter8.py

---
+++ hunter8.py
@@ -0,0 +1,10 @@
+ def loops():
+	n=int(input())
+	l=[]
+	for i in range(n):
+		l.append(int(input()))
+	for i in range(n):
+		for j in range(i+1,n):
+			for k in range(j+1,n):
+				if l[i]+l[j]==l[k]:
+					print(l[i],l[j],l[k])
