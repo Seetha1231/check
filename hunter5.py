8c3f3d7998c0788f585a3d749a5b22d3d30a323a
Seetha1231 <34570365+Seetha1231@users.noreply.github.com>
2018-01-04 17:28:54 +0530

    Create hunter5.py

---
+++ hunter5.py
@@ -0,0 +1,17 @@
+def f():
+	s=input()
+	d=''
+	c=1
+	r=[]
+	for i in range(len(s)-1):
+		v=int(s[i])
+		if v<26:
+			d=s[i]+s[i+1]
+			if v>26:
+				continue
+			c=c+1
+	print(c)
+try:
+  f()
+except:
+  print('invalid')
