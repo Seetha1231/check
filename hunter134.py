31ac6d342cf7c595d3e1422d7f80c2690fae804d
Seetha1231 <34570365+Seetha1231@users.noreply.github.com>
2018-01-04 18:06:28 +0530

    Create hunter134.py

---
+++ hunter134.py
@@ -0,0 +1,15 @@
+def ifcop(n):
+	flag=0
+	for i in range(2,int(n/2)+1):
+		if n%i ==0:
+			return 0
+	return 1
+def coprime():
+	n=int(input())
+	m=int(input())
+	f1=ifcop(m)
+	f2=ifcop(n)
+	if f1==1 and f2==1 :
+		print('Yes')
+	else :
+		print('No')
