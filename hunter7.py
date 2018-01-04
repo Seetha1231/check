5c3f8ad100f5627280970377883c80c27c9ab54f
Seetha1231 <34570365+Seetha1231@users.noreply.github.com>
2018-01-04 17:40:07 +0530

    Create hunter7.py

---
+++ hunter7.py
@@ -0,0 +1,13 @@
+def oddeven():
+	n=int(input())
+	l=[]
+	for i in range(n):
+		l.append(int(input()))
+	o,e=[],[]
+	for i in range(n):
+		if i%2==0 and l[i]%2!=0:
+			o.append(l[i])
+		elif i%2!=0 and l[i]%2==0:
+			o.append(l[i])
+	print(o)
+  
