7591866ffe6ba200343c8951c59cd291f40ed2cc
Seetha1231 <34570365+Seetha1231@users.noreply.github.com>
2018-01-06 22:14:47 +0530

    Create hun_15.py

---
+++ hun_15.py
@@ -0,0 +1,12 @@
+def perm(a,k=0):
+	if(k==len(a)):
+		print (a)
+	else:
+		for i in range(k,len(a)):
+			a[k],a[i] = a[i],a[k]
+			perm(a, k+1)
+			a[k],a[i] = a[i],a[k]
+try:
+	perm()
+except:
+	print('Invalid')
