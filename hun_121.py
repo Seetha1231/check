df7d3780eda936136c9659fdefb6bd5b05c0bef5
Seetha1231 <34570365+Seetha1231@users.noreply.github.com>
2018-01-26 15:22:41 +0530

    Create hun_121.py

---
+++ hun_121.py
@@ -0,0 +1,32 @@
+def hun_121():
+	st=input()
+	n=len(st)
+	table=[[0 for i in range(n)] for j in range(n)]
+	max=1
+	i=0
+	while(i<n-1):
+		table[i][i]=True
+		i+=1
+	start=0
+	i=0
+	while(i<n-1):
+		if st[i]==st[i+1]:
+			max=2
+			statr=i
+			table[i][i+1]=True
+		i+=1
+	k=3
+	while(k<=n):
+		i=0
+		while(i<(n-k+1)):
+			j=i+k-1
+			if table[i+1][j-1]==True and st[i]==st[j]:
+				table[i][j]=True
+				if max<k:
+					max=k
+					start=i
+			i+=1
+		k+=1
+	print(st[start:max])
+  
+hun_121()
