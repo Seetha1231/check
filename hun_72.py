e75a389a5669c81bdf231e1ee603be3867a7a229
Seetha1231 <34570365+Seetha1231@users.noreply.github.com>
2018-03-20 20:07:12 +0530

    Create hun_72.py

---
+++ hun_72.py
@@ -0,0 +1,21 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      SEETHALAKSHMI
+#
+# Created:     26-02-2018
+# Copyright:   (c) SEETHALAKSHMI 2018
+# Licence:     <your licence>
+#------------------------------------------------------------------------------)
+def main():
+    l=list(map(str,input().split()))
+    for i in range(0,len(l),2):
+        s=l[i]
+        l[i]=s[::-1]
+    print(l)
+
+
+
+if __name__ == '__main__':
+    main()
